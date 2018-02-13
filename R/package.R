b8efe9a0d89d74b669173e27fbc430e903824c8e
Henrik Bengtsson <henrikb@braju.com>
2018-02-13 11:56:10 +0000

    version 0.2.0

---
+++ R/package.R
@@ -0,0 +1,18 @@
+#' future.callr: A Future for callr
+#'
+#' The \pkg{future.callr} package implements the Future API
+#' on top of \pkg{callr}.
+#' The Future API is defined by the \pkg{future} package.
+#'
+#' To use callr futures, load \pkg{future.callr}, and
+#' select the type of future you wish to use, e.g. `plan(callr)`.
+#'
+#' @examples
+#' \donttest{
+#' plan(callr)
+#' demo("mandelbrot", package = "future", ask = FALSE)
+#' }
+#'
+#' @docType package
+#' @name future.callr
+NULL
